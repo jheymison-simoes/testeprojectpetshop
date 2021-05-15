d152cf86bbdecfae7d4335d376087532dc98bd4f
Projeto Petshop <projectpetshop2021@gmail.com>
2021-05-15 15:37:02 -0300

    Alterado url Back end

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://backedprojectpet.herokuapp.com',
+    baseURL: 'http://projectpeshop-com-br.umbler.net/',
 });
 
 export default api;
